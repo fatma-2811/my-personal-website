da27f334b643f480c3d8dbbd877593b5385ef76d
fatma-2811 <fatmaalzhraa437@gmail.com>
2026-03-24 16:59:29 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,20 +3,28 @@ console.log('age: 21 ');
 console.log('ID: 420418154 ');
 console.log('phone: 0592878918 ');
 console.log('Adress: Rafah ');
+// let nav_item= document.querySelectorAll(".nav-item")
+// window.onscroll = function () {
+//     if (window.scrollY > 500) {
+//         nav_item.style.color = "white";
+      
+
+//     }
+//     else {
+//         nav_item.style.color = "#07A097";
+
+//     }
+// };
 let navbar = document.querySelector(".navbar");
 window.onscroll = function () {
-    if (window.scrollY > 20) {
+    if (window.scrollY > 500) {
         navbar.style.backgroundColor = "#07A097";
       
 
     }
     else {
         navbar.style.backgroundColor = "white";
-
+  
     }
 };
-// alert('Welcome in  profile websites')
-// confirm
-// let names= prompt(" Enter your Name:")
-// console.log(names);
-confirm("Ali You:")
+
