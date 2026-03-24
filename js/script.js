8bcde46ea93d93904d5b4b99b127713979d0bdf0
fatma-2811 <fatmaalzhraa437@gmail.com>
2026-03-24 17:07:37 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+console.log('Name: Fatma Alzhra Akram Abu Mohsen');
+console.log('age: 21 ');
+console.log('ID: 420418154 ');
+console.log('phone: 0592878918 ');
+console.log('Adress: Rafah ');
+// let nav_item= document.querySelectorAll(".nav-item")
+// window.onscroll = function () {
+//     if (window.scrollY > 500) {
+//         nav_item.style.color = "white";
+      
+
+//     }
+//     else {
+//         nav_item.style.color = "#07A097";
+
+//     }
+// };
+let navbar = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (window.scrollY > 500) {
+        navbar.style.backgroundColor = "#07A097";
+      
+
+    }
+    else {
+        navbar.style.backgroundColor = "white";
+  
+    }
+};
+
